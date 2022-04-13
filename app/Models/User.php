2efdd06bb2168f4c8e2ede8ef7f2046b4c53742f
jmonloop <monleonlopezjavier@gmail.com>
2022-04-13 19:36:49 +0200

    models created

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,37 +6,59 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+
+use Laravel\Passport\HasApiTokens;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    public function message (){
+        return $this -> hasMany(Message::class);
+    }
+
+    public function membership (){
+        return $this -> hasMany(Membership::class);
+    }
+
+    public function parties () {
+        return $this->hasMany(Party::class);
+    }
+
+    public function Game () {
+        return $this->hasMany(Game::class);
+    }
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'steamId',
+        'nickName'
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'isAdmin',
+        'isActive'
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
